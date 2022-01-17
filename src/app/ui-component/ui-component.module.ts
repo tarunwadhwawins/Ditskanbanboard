9705255ba698685d242890c8e66d922f3cc1cb75
tarun <tarunwadhwawins@gmail.com>
2022-01-17 10:22:33 +0530

    ui changes

--- src/app/ui-component/ui-component.module.ts
+++ src/app/ui-component/ui-component.module.ts
@@ -3,16 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { UiComponentRoutingModule } from './ui-component-routing.module';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     UiComponentRoutingModule,
-    NzDropDownModule
+    NzDropDownModule,
+    NzIconModule,
+    NzInputModule
   ],
   exports: [
-    NzDropDownModule
+    NzDropDownModule,
+    NzIconModule,
+    NzInputModule
   ]
 })
 export class UiComponentModule { }
