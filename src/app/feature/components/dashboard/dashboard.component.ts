ad50d60c8033144e09f9a4b52de480a3ca5e0032
tarun wadhwa <tarunwadhwawins@gmail.com>
2022-01-20 10:34:54 +0530

    new changes

--- src/app/feature/components/dashboard/dashboard.component.ts
+++ src/app/feature/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -8,56 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   trackIds:any;
-  mainTrackArr :any[] = [
-  {
-    "title": "Todo",
-    "id": "todo",
-    "tasks": [
-      {
-        "id": "first-task",
-        "title": "First Task",
-        "description": "This is my first task"
-      }
-    ]
-  },
-  {
-    "title": "In Progress",
-    "id": "inprogress",
-    "tasks": [
-      {
-        "id": "seconf-task",
-        "title": "Second Task",
-        "description": "This is my first task"
-      }
-    ]
-  },
-  {
-    "title": "D-Done",
-    "id": "ddone",
-    "tasks": [
-      {
-        "id": "third-task",
-        "title": "Third Task",
-        "description": "This is my first task"
-      }
-    ]
-  },
-  {
-    "title": "QA Pass",
-    "id": "qapass",
-    "tasks": [
-      {
-        "id": "fourth-task",
-        "title": "Fourth Task",
-        "description": "This is my first task"
-      }
-    ]
-  }
-]
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  todo = ['Task 12', 'Task 13', 'Task 14', 'Task 15'];
+  doing = ['Task 7', 'Task 8', 'Task 9', 'Task 10', 'Task 11'];
+  done = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 6'];
 
   onTaskDrop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) {
@@ -67,6 +27,19 @@ export class DashboardComponent implements OnInit {
         event.container.data,
         event.previousIndex,
         event.currentIndex);
+    }
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
     }
   }
   
